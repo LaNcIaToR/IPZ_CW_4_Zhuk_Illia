9f3b76f4df03ef24033590d007e99852533cf766
LaNcIaToR <jonas7238@icloud.com>
2024-03-14 14:36:15 +0200

    add class for Task object

--- app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
@@ -1,15 +1,40 @@
 package ua.edu.lntu.cw_4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_4.ui.theme.IPZ_CW_4_Zhuk_IlliaTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,3 +53,11 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val date: String,
+    val status: TaskStatus
+)
